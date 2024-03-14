27ca481190913d076f0bd3c34804cb4447da0c46
Nicolas Antônio <139736216+Nikao777@users.noreply.github.com>
2024-03-14 10:38:02 -0300

    Aula 13

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,18 @@
+let c = 1
+do {
+  console.log (`Passo: ${c}`)
+  c++
+} while (c <= 6)
+
+/*let c = 1
+while(c <= 500){
+  console.log(`Passo: ${c}`)
+  c++
+}*/
+
+/*console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')*/
