b864d525d870d2b74c20e8dd67a0758e53c7ed36
Nicolas Antônio <139736216+Nikao777@users.noreply.github.com>
2024-03-14 15:34:25 -0300

    Atividade 17

--- Aula 14ex/exercício017/script.js
+++ Aula 14ex/exercício017/script.js
@@ -1,16 +1,18 @@
 function contar(){
   let num = document.getElementById('num').value
   let res = document.getElementById('res')
+  const localt = document.getElementById('localt')
 
-  for (let n = 1; n <= 10; n++) {
-    let numero = Number(num)
-    res.innerHTML += `
-    <table>
-      <tbody>
-        <tr>
-          <td>${numero} x ${n} = ${num*n} 
-        </tr>
-      </tbody>
-    </table>`
+  if (num == ''){
+    alert('Digite um Número')
+  } else{
+    localt.innerHTML = ''
+    for (let n = 1; n <= 10; n++) {
+      let numero = Number(num)
+      let item = document.createElement('option')
+      item.text = `${numero} x ${n} = ${num*n}`
+      item.value = `tab${n}`
+      localt.appendChild(item)
+    }
   }
 }
