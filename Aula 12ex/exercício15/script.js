229cc036dc0a0c4f0fea540091ca33470f91de27
Nicolas Antônio <139736216+Nikao777@users.noreply.github.com>
2024-03-14 01:24:51 -0300

    Atividade 2

--- Aula 12ex/exercício15/script.js
+++ Aula 12ex/exercício15/script.js
@@ -1,21 +1,3 @@
-let img = document.getElementById('imagem')
-let msg = document.getElementById('msg')
-const data = new Date()
-const hora = data.getHours()
-
-function carregar(){
-  msg.innerHTML = `<p class= "txt">Agora são ${hora} Horas</p>`;
-  if (hora > 5 && hora < 12){
-  //Bom Dia!
-    img.src = './img/manha.png'
-    document.body.style.background = '#fa8f55'
-  }else if (hora >= 12 && hora < 18){
-    //Boa Tarde!
-    img.src = './img/tarde.png'
-    document.body.style.background = '#f6794d'
-  }else if(hora >= 18 || hora <= 5){
-    //Boa Noite!
-    img.src = './img/noite.png'
-    document.body.style.background = '#002665'
-  }
+function verificar(){
+  
 }
