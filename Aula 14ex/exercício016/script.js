f6e0fef8e67871cdbc0ff7cce3cbfbac32572b31
Nicolas Antônio <139736216+Nikao777@users.noreply.github.com>
2024-03-14 14:46:47 -0300

    Atividade 15 e 16

--- Aula 14ex/exercício016/script.js
+++ Aula 14ex/exercício016/script.js
@@ -1,3 +1,85 @@
-function verificar(){
-  
+function contar(){
+  let inicio = document.getElementById('inicio').value
+  let fim = document.getElementById('fim').value
+  let pulo = document.getElementById('pulo').value
+  let res = document.getElementById('res')
+
+  if (inicio.lenght == 0 || fim.lenght == 0 || pulo.lenght == 0){
+    alert('[ERRO]')
+  } else {
+    let i = Number(inicio)
+    let f = Number(fim)
+    let p = Number(pulo)
+    if (p <= 0){
+      p = 1
+    }
+    if (i < f){
+      for (let c = i; c <= f; c += p){
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    } else {
+      for (let c = i; c >= f; c -= p){
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*if (inicio.lenght == 0 || pulo == 0 || fim.lenght == 0){
+    //alert('[ERRO] Você deve ter esquecido de digitar Algum Número, por favor, digite e tente novamente!')
+  } else{
+    res.innerHTML = `Contando... <br/>`
+    let i = Number(inicio)
+    let f = Number(fim)
+    let p = Number(pulo)
+
+    if (p <= 0){
+      alert('Passo Inválido, considerando Passo = 1');
+      p = 1
+    }
+
+    if (i < f){
+      //Contagem Crescente
+      for (let c = i; c <= f; c += p){
+        res.innerHTML += `${c} \u{1F449} `
+      }
+    } else {
+      //Contagem Decrescente
+      for(let c = i; c >= f; c -= p){
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`
+  }*/
 }
--- Aula 14ex/exercício017/script.js
+++ Aula 14ex/exercício017/script.js
@@ -1,3 +1,16 @@
-function verificar(){
-  
+function contar(){
+  let num = document.getElementById('num').value
+  let res = document.getElementById('res')
+
+  for (let n = 1; n <= 10; n++) {
+    let numero = Number(num)
+    res.innerHTML += `
+    <table>
+      <tbody>
+        <tr>
+          <td>${numero} x ${n} = ${num*n} 
+        </tr>
+      </tbody>
+    </table>`
+  }
 }
