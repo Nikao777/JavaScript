247b07a8e4d6b17d7b19c3189d0d4fc3abf5833f
Nicolas Antônio <139736216+Nikao777@users.noreply.github.com>
2024-03-19 04:30:33 -0300

    object

--- Aula 16ex/ex18/script.js
+++ Aula 16ex/ex18/script.js
@@ -1,6 +1,6 @@
-let num = document.getElementById('num').value
-let itens = document.getElementById('itens')
-let result = document.getElementById('res')
+let num = document.getElementById('num')
+let lista = document.getElementById('lista')
+let localresult = document.getElementById('res')
 let valores = []
 
 function validarnum (n) {
@@ -11,19 +11,54 @@ function validarnum (n) {
   }
 }
 
-function inList(n, l) {
-  if (l.indexOf(n) != -1){
+function inLista(n, l) {
+  if (l.indexOf(Number(n)) != -1) {
     return true
   } else {
-    return false
+    return false  
   }
 }
 
 
 function adicionar() {
-  if (validarnum(num) && !inList(num, itens))
-    alert('Tudo Ok!')
-    
-} else {
-  alert('O Número que digitou é inválido ou já foi digitado anteriormente')
+  if (validarnum(num.value) && !inLista(num.value, valores)) {
+    valores.push(Number (num.value))
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} adiconado`
+    lista.appendChild(item)
+    res.innerHTML = ''
+  } else {
+    alert('O número foi inválido ou já foi adicionado')
+  }
+  num.value = ''
+  num.focus() //quando digitar um número e adicionar, ele vai voltar o cursor para o local de adicionar número
+}
+
+function finalizar(){
+  if (valores.length == 0){
+    alert('Adicione um número')
+  } else {
+    let tot = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
+
+    for (let pos in valores) {
+      soma += valores[pos]
+      if (valores[pos] > maior)
+        maior = valores[pos]
+      if (valores [pos] < menor)
+        menor = valores[pos] 
+    }
+
+    media = soma / tot
+
+    localresult.innerHTML = ''
+    localresult.innerHTML += `<p>Ao Todo, temos ${tot} números adicionados</p><br/>`
+    localresult.innerHTML += `<p>O maior valor informado foi ${maior}</p> <br/>`
+    localresult.innerHTML += `<p>O menor valor informado foi ${menor}</p> <br/>`
+    localresult.innerHTML += `A soma dos valores é ${soma} <br/>`
+    localresult.innerHTML += `<br/> A média dos valores é ${media}`
+  }
 }
